2708193bf14293e53023bcbdc1509a7aee34919b
reinier303 <30725987+reinier303@users.noreply.github.com>
2020-04-23 12:27:53 +0200

    Added EpicToon and some sexy scripts

---
+++ PurgersOfTheCrystalWatchers/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerCombat RPlayerCombat;
+    public PlayerMovement RPlayerMovement;
+    public PlayerEntity RPlayerEntity;
+
+    [HideInInspector] public bool Attacking;
+
+    private void Awake()
+    {
+        RPlayerCombat = GetComponent<PlayerCombat>();
+        RPlayerMovement = GetComponent<PlayerMovement>();
+        RPlayerEntity = GetComponent<PlayerEntity>();
+    }
+}
