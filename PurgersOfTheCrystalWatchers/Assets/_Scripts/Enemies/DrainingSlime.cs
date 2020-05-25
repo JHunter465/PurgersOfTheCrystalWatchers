2951c35ba08a6ee588d259ea8d5e51799ba12e35
reinier303 <30725987+reinier303@users.noreply.github.com>
2020-05-25 18:12:10 +0200

    Added slime

--- PurgersOfTheCrystalWatchers/Assets/_Scripts/Enemies/DrainingSlime.cs
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Enemies/DrainingSlime.cs
@@ -5,7 +5,15 @@ namespace POTCW
 {
     public class DrainingSlime : BaseEnemy
     {
-        [SerializeField] private float drainDistance;
+        [SerializeField] private float drainDistance, gooSpawnTime;
+
+        private ObjectPooler objectPooler;
+
+        private void Start()
+        {
+            objectPooler = ObjectPooler.Instance;
+            StartCoroutine(SpawnGoo());
+        }
 
         protected override void Update()
         {
@@ -13,8 +21,22 @@ namespace POTCW
             DrainCrystal();
         }
 
+        public IEnumerator SpawnGoo()
+        {
+            Vector3 position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+            Vector3 rotation = new Vector3(0, Random.Range(0, 360), 0);
+            Debug.Log(objectPooler.PoolDictionary["FeetGnawer"]);
+            objectPooler.SpawnFromPool("Goo", position, Quaternion.Euler(rotation));
+            yield return new WaitForSeconds(gooSpawnTime);
+            StartCoroutine(SpawnGoo());
+        }
+
         private void DrainCrystal()
         {
+            if(player == null)
+            {
+                return;
+            }
             if(Vector3.Distance(transform.position, player.position) < drainDistance)
             {
 
---
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Enemies/Goo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Invector.vCharacterController;
+using Invector;
+
+public class Goo : MonoBehaviour
+{
+    public float DisableTime, Damage;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(DisableAfterTime());
+    }
+    
+    private IEnumerator DisableAfterTime()
+    {
+        yield return new WaitForSeconds(DisableTime);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        vThirdPersonController player = other.GetComponent<vThirdPersonController>();
+        if (player != null)
+        {
+            Invector.vDamage damage = new vDamage((int)Damage);
+            player.TakeDamage(damage);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
