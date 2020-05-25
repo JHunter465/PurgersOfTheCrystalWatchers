e2378012284ac89ceebebcc3dcc421c7c0526c44
reinier303 <30725987+reinier303@users.noreply.github.com>
2020-05-25 11:51:22 +0200

    Changed Projectile

---
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/BossBehaviour/Projectiles/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float ProjectileSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * ProjectileSpeed * Time.deltaTime;
+    }
+}
