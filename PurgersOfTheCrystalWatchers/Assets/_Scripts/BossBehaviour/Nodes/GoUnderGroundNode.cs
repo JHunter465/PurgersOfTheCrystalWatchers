ab853d4ff9bb32afaf5e1a9d2d085130dacc7bc4
Bas de Koningh <Bas.ghosts@gmail.com>
2020-06-05 15:24:42 +0200

    Bug fixes / Open Terrain Area / Added some little tweaks / summon spawn update

--- PurgersOfTheCrystalWatchers/Assets/_Scripts/BossBehaviour/Nodes/GoUnderGroundNode.cs
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/BossBehaviour/Nodes/GoUnderGroundNode.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e40a33e06aaee0d4cfde16d13655c831a3738849808146fdeabb63a9b134146e
-size 1949
+oid sha256:5ac7621e4724da9ee6034d6c1c2225e9bdfc6a20ca738b43b2fc5aacc5db356e
+size 1995
