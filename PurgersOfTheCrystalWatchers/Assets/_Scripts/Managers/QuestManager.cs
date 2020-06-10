eca15262c46990bbcc2681d06f27e3c481ad1040
Bas de Koningh <Bas.ghosts@gmail.com>
2020-06-10 10:33:29 +0200

    Ganfaul imported / new player character / Rig works and made a prefab called: NewPlayer

--- PurgersOfTheCrystalWatchers/Assets/_Scripts/Managers/QuestManager.cs
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Managers/QuestManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fde74702a171c4ac94c5e22275c60f99e8519f86c9167ff98662acad09ae556
-size 3713
+oid sha256:7b6bb34ffbc62db2dae370a790f5198d9d34cb2f8a4e397f2385a25037cc465a
+size 3758
