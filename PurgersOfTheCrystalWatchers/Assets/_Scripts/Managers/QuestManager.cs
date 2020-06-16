7c84670b1813812e1068adaa124da1c50b98d2cb
Bas de Koningh <Bas.ghosts@gmail.com>
2020-06-16 13:06:42 +0200

    Update / Tweaks/ Starting tutorial level / Need up to date merch of development

--- PurgersOfTheCrystalWatchers/Assets/_Scripts/Managers/QuestManager.cs
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Managers/QuestManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b6bb34ffbc62db2dae370a790f5198d9d34cb2f8a4e397f2385a25037cc465a
-size 3758
+oid sha256:9ca5d8e8bc17d9fed1ba75a6f7ac480a05e99bac01ccd593fe89a98bab1d6cc4
+size 3916
