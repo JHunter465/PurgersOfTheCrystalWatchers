958039fcd619cc37d036a5c118822122e7b8260d
Bas de Koningh <Bas.ghosts@gmail.com>
2020-06-16 16:13:02 +0200

    Tutorial Level Setup

--- PurgersOfTheCrystalWatchers/Assets/_Scripts/Managers/QuestManager.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9ca5d8e8bc17d9fed1ba75a6f7ac480a05e99bac01ccd593fe89a98bab1d6cc4
-size 3916
---
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Quests/QuestManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dcde10bfb6c977e6a8ad94c8574f755228ac899a96e7d0a4bc6a4816de72eff5
+size 5620
