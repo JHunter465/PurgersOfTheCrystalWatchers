c1ad11821fd667a6878b6e7e8ed2a08e8a347723
Bas de Koningh <Bas.ghosts@gmail.com>
2020-06-17 12:10:55 +0200

    Update / reorderable list added

---
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Quests/Editor/CustomQuestManagerEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dcd5023717f535b1936a7c2e4d30e8a262a090ec192617f5df1f3ad85ac3bc97
+size 673
--- PurgersOfTheCrystalWatchers/Assets/_Scripts/Quests/QuestManager.cs
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Quests/QuestManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcde10bfb6c977e6a8ad94c8574f755228ac899a96e7d0a4bc6a4816de72eff5
-size 5620
+oid sha256:d945e5113ddca95cff417efdb429e4af22d1756dc1196ad6338e14bd8b8792b4
+size 6065
