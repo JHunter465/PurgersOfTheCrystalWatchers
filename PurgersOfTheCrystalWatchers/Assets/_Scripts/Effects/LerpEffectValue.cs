d54dfcc79a2753441211663109983ffd1ecf1439
reinier303 <30725987+reinier303@users.noreply.github.com>
2020-06-18 13:10:31 +0200

    Fixed reviving visual bug and explosion damage bug

--- PurgersOfTheCrystalWatchers/Assets/_Scripts/Effects/LerpEffectValue.cs
+++ PurgersOfTheCrystalWatchers/Assets/_Scripts/Effects/LerpEffectValue.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8214638c22db1f9d49dccf721c6f7105265db189166a904025e98a601d82eb9
-size 1260
+oid sha256:e3643c24042d8bf73fbf89bfb354904bda2a9478c258b4a99eaf80718da98fe3
+size 1258
